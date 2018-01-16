e8d883c7a5bcb3a529ac7b5c27c7f4feae713b22
gittegylet <gbruno2000@gmail.com>
2018-01-16 13:11:33 +0100

    wasItABomb + isAnyNonBombFieldStillHidden függvények

---
+++ src/isAnyNonBombFieldStillHidden.js
@@ -0,0 +1,21 @@
+﻿
+function isAnyNonBombFieldStillHidden(aknamezo) {
+
+    var x1 = 0;
+    aknamezo.forEach(function (row){
+        x1 += row.filter(function (item) {
+
+            return item < 0 && item > -12;
+
+        }).length;
+
+    });
+
+
+    return x1 == 0;
+
+};
+
+
+
+exports.isAnyNonBombFieldStillHidden = isAnyNonBombFieldStillHidden;
